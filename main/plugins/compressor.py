d07e9d1b496e60a76bfcd22fe062377c2059e550
Arifmetik <100046368+Arifmetik@users.noreply.github.com>
2022-03-18 19:01:04 +0500

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -1,11 +1,18 @@
-#TG:ChauhanMahesh/DroneBots
-#Github.com/vasusen-code
+#  This file is part of the VIDEOconvertor distribution.
+#  Copyright (c) 2021 vasusen-code ; All rights reserved. 
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  General Public License for more details.
+#
+#  License can be found in < https://github.com/vasusen-code/VIDEOconvertor/blob/public/LICENSE> .
 
-import asyncio
-import time
-import subprocess
-import re
-import os
+import asyncio, time, subprocess, re, os, ffmpeg
 from datetime import datetime as dt
 from .. import Drone, BOT_UN, LOG_CHANNEL
 from telethon import events
@@ -17,9 +24,11 @@ from telethon.errors.rpcerrorlist import MessageNotModifiedError
 from telethon.tl.types import DocumentAttributeVideo
 from main.plugins.actions import LOG_START, LOG_END
 
-async def compress(event, msg):
+async def compress(event, msg, ffmpeg_cmd=0, ps_name=None):
+    if ps_name is None:
+        ps_name = '**COMPRESSING:**'
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Process boshlandi", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=msg.id)
     new_name = "out_" + dt.now().isoformat("_", "seconds")
     if hasattr(msg.media, "document"):
         file = msg.media.document
@@ -43,29 +52,66 @@ async def compress(event, msg):
         ext = (n.split("."))[1]
         out = new_name + ext
     DT = time.time()
-    log = await LOG_START(event, f'**Siqish boshlandi**\n\nBot navbat bilan ishlaydi')
-    log_end_text = f'**Siqish yakunlandi**\n\nBot hozirda tekin'
+    _ps = "COMPRESS"
+    if ps_name != "**COMPRESSING:**":
+        _ps = "ENCODE"
+    log = await LOG_START(event, f'**{str(_ps)} PROCESS STARTED**\n\n[Bot is busy now]({SUPPORT_LINK})')
+    log_end_text = f'**{_ps} PROCESS FINISHED**\n\n[Bot is free now]({SUPPORT_LINK})'
     try:
-        await fast_download(n, file, Drone, edit, DT, "**Yuklanmoqda:**")
+        await fast_download(n, file, Drone, edit, DT, "**DOWNLOADING:**")
     except Exception as e:
-        os.rmdir("compressmedia")
+        os.rmdir("encodemedia")
         await log.delete()
         await LOG_END(event, log_end_text)
         print(e)
-        return await edit.edit(f"Yuklashda xatolik!\n\nMurojat: [ADMIN]({SUPPORT_LINK})", link_preview=False) 
+        return await edit.edit(f"An error occured while downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False) 
     name = '__' + dt.now().isoformat("_", "seconds") + ".mp4"
     os.rename(n, name)
+    await edit.edit("Extracting metadata...")
+    vid = ffmpeg.probe(name)
+    codec = vid['streams'][0]['codec_name']
+    hgt = video_metadata(name)["height"]
+    wdt = video_metadata(name)["width"]
+    if ffmpeg_cmd == 2:
+        if hgt == 360 or wdt == 640:
+            await log.delete()
+            await LOG_END(event, log_end_text)
+            await edit.edit("Fast compress cannot be used for this media, try using HEVC!")
+            os.rmdir("encodemedia")
+            return
+    if ffmpeg_cmd == 3:
+        if codec == 'hevc':
+            await log.delete()
+            await LOG_END(event, log_end_text)
+            await edit.edit("The given video is already in H.265 codec.")
+            os.rmdir("encodemedia")
+            return
+    if ffmpeg_cmd == 4:
+        if codec == 'h264':
+            await log.delete()
+            await LOG_END(event, log_end_text)
+            await edit.edit("The given video is already in H.264 codec.")
+            os.rmdir("encodemedia")
+            return
     FT = time.time()
     progress = f"progress-{FT}.txt"
-    cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -crf 28 -acodec copy """{out}""" -y'
+    cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" None """{out}""" -y'
+    if ffmpeg_cmd == 1:
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -crf 28 -acodec copy """{out}""" -y'
+    elif ffmpeg_cmd == 2:
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -c:v libx265 -crf 22 -preset ultrafast -s 640x360 -c:a copy """{out}""" -y'
+    elif ffmpeg_cmd == 3:
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -crf 18 -acodec copy """{out}""" -y'
+    elif ffmpeg_cmd == 4:
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx264 -crf 18 -acodec copy """{out}""" -y'
     try:
-        await ffmpeg_progress(cmd, name, progress, FT, edit, '**Siqilmoqda:**')
+        await ffmpeg_progress(cmd, name, progress, FT, edit, ps_name, log=log)
     except Exception as e:
         await log.delete()
         await LOG_END(event, log_end_text)
-        os.rmdir("compressmedia")
+        os.rmdir("encodemedia")
         print(e)
-        return await edit.edit(f"Qandaydir xatolik!\n\nMurojat: [ADMIN]({SUPPORT_LINK})", link_preview=False)  
+        return await edit.edit(f"An error occured while FFMPEG progress.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)  
     out2 = dt.now().isoformat("_", "seconds") + ".mp4" 
     if msg.file.name:
         out2 = msg.file.name
@@ -74,28 +120,31 @@ async def compress(event, msg):
     os.rename(out, out2)
     i_size = os.path.getsize(name)
     f_size = os.path.getsize(out2)     
-    text = f'**Siqildi**: @{BOT_UN} da\n\nSiqishdan oldin: `{i_size/1000000}` mb\nSiqishdan keyin: `{f_size/1000000}` mb'
+    text = F'**ENCODED by:** @{BOT_UN}'
+    if ps_name != "**ENCODING:**":
+        text = f'**COMPRESSED by** : @{BOT_UN}\n\nbefore compressing : `{i_size}`\nafter compressing : `{f_size}`'
     UT = time.time()
+    await log.edit("Uploading file.")
     if 'x-matroska' in mime:
         try:
-            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**Yuborilmoqda:**')
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
             await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
         except Exception as e:
             await log.delete()
             await LOG_END(event, log_end_text)
-            os.rmdir("compressmedia")
+            os.rmdir("encodemedia")
             print(e)
-            return await edit.edit(f"Yuborishda xatolik!\n\nMurojat: [ADMIN]({SUPPORT_LINK})", link_preview=False)
+            return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
     elif 'webm' in mime:
         try:
-            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**Yuborilmoqda:**')
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
             await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
         except Exception as e:
             await log.delete()
             await LOG_END(event, log_end_text)
-            os.rmdir("compressmedia")
+            os.rmdir("encodemedia")
             print(e)
-            return await edit.edit(f"Yuborishda xatolik!\n\nMurojat: [ADMIN]({SUPPORT_LINK})", link_preview=False)
+            return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
     else:
         metadata = video_metadata(out2)
         width = metadata["width"]
@@ -103,24 +152,21 @@ async def compress(event, msg):
         duration = metadata["duration"]
         attributes = [DocumentAttributeVideo(duration=duration, w=width, h=height, supports_streaming=True)]
         try:
-            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**Yuborilmoqda:**')
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
             await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG3, attributes=attributes, force_document=False)
         except Exception:
             try:
-                uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**Yuborilmoqda:**')
+                uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
                 await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
             except Exception as e:
                 await log.delete()
                 await LOG_END(event, log_end_text)
-                os.rmdir("compressmedia")
+                os.rmdir("encodemedia")
                 print(e)
-                return await edit.edit(f"Yuborishda xatolik!\n\nMurojat: [ADMIN]({SUPPORT_LINK})", link_preview=False)
+                return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
     await edit.delete()
     os.remove(name)
     os.remove(out2)
     await log.delete()
-    log_end_text2 = f'**Process yakunlandi**\n\nKetgan vaqt: {round((time.time()-DT)/60)} minut\nOldingi hajm: {i_size/1000000}mb.\nKeyingi hajm: {f_size/1000000}mb.\n\nBot hozircha tekin'
+    log_end_text2 = f'**{_ps} PROCESS FINISHED**\n\nTime Taken: {round((time.time()-DT)/60)} minutes\nInitial size: {i_size/1000000}mb.\nFinal size: {f_size/1000000}mb.\n\n[Bot is free now.]({SUPPORT_LINK})'
     await LOG_END(event, log_end_text2)
-    
-
-
